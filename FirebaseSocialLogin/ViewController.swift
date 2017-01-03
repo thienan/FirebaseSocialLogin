8755702ed4c0b4d154730f8140126a7e006506b8
문화창조아카데미38 <CELAcademy38@munhwachangjoakademi38ui-MacBook-Pro.local>
2017-01-03 20:54:45 +0900

    first commit

---
+++ FirebaseSocialLogin/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  FirebaseSocialLogin
+//
+//  Created by 문화창조아카데미38 on 2017. 1. 3..
+//  Copyright © 2017년 gipark. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+import Firebase
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let loginButton = FBSDKLoginButton()
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        
+        loginButton.delegate = self
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        
+        print("Successfully logged in with facebook...")
+    }
+}
+
